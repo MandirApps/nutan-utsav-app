c76ad4164bb897cd7ee9dfff6d1e7a3ddec7428a
Pritam Sangani <pritam.sangani@thoughtworks.com>
2021-09-18 21:57:48 +0100

    removes db and auth related files

--- types.ts
+++ types.ts
@@ -1,5 +1,4 @@
-import { DefaultCtx, SessionContext, SimpleRolesIsAuthorized } from "blitz"
-import { User } from "db"
+import { DefaultCtx, SessionContext } from "blitz"
 
 // Note: You should switch to Postgres and then use a DB enum for role type
 export type Role = "ADMIN" | "USER"
@@ -7,12 +6,5 @@ export type Role = "ADMIN" | "USER"
 declare module "blitz" {
   export interface Ctx extends DefaultCtx {
     session: SessionContext
-  }
-  export interface Session {
-    isAuthorized: SimpleRolesIsAuthorized<Role>
-    PublicData: {
-      userId: User["id"]
-      role: Role
-    }
   }
 }
